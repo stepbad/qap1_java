60901ce0361762167cd8f1a6f7053f68cbd8d950
Stephen Badcock <badcockstephen73@gmail.com>
2025-01-30 10:12:04 -0330

    final version qap1
    last updates for qap1, finished and uploaded.

--- TestTime.java
+++ TestTime.java
@@ -13,5 +13,15 @@ public class TestTime {
         System.out.println("After Changes:");
         System.out.println("Time 1: " + t1);
         System.out.println("Time 2: " + t2);
+
+        Time t3 = new Time(21, 59, 59);
+        Time t4 = new Time(10, 00, 00);
+
+        t3.nextSecond();
+        t4.previousSecond();
+
+        System.out.println("Really test next and previous:");
+        System.out.println("new Time 3: " + t3);
+        System.out.println("new Time 4: " + t4);
     }
 }
